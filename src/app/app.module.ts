f104de735ec3ba41263e85fdde39885fde74c07d
Ayyoub Meijer <ayyoubmeijer@hotmail.com>
2020-04-07 21:19:20 +0200

    + all pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,51 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { RouterModule, Routes } from '@angular/router';
+
+//material
+import { MatToolbarModule } from '@angular/material/toolbar';
+
+import { HomeComponent } from './home/home.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { ProjectComponent } from './project/project.component';
+import { ContactComponent } from './contact/contact.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  { path: 'over-mij', component: AboutMeComponent },
+  { path: 'projecten', component: ProjectComponent },
+  { path: 'contact', component: ContactComponent },
+  {
+    path: '',
+    component: HomeComponent,
+    data: { title: 'Hai' }
+  },
+  { path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
+
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutMeComponent,
+    ProjectComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    ),
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
